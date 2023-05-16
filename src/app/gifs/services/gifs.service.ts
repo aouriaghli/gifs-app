8138a46ef9a300bdc271a859b387e28aa7eeaad4
Ayoub Ouriaghli Romeo <ayoub.ouriaghli@soprasteria.com>
2023-05-16 15:55:25 +0200

    Sec.8: ViewChild,Giphy API, localstorage

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+@Injectable({providedIn: 'root'})
+export class GifsService {
+
+  public gifList: Gif[] = [];
+
+  private _tagsHistory: string[] = [];
+  private apiKey:       string = 'Lagnve7OTCUw2mIspAQcZCff0Hn7KaAL';
+  private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http:HttpClient) {
+    this.loadLocalStorage();
+    console.log('Gifs Service Ready');
+  }
+
+  get tagsHistory(){
+    return [...this._tagsHistory]; //
+  }
+
+
+  private organizeHistory(tag: string){
+    tag = tag.toLocaleLowerCase();
+
+    if (this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter((oldTag => oldTag !== tag));
+    }
+    this._tagsHistory.unshift(tag);//añadir el tag al principio de la lista;
+    this._tagsHistory = this._tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void{
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if ( !localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!); // ! non-null operator
+
+    if (this._tagsHistory.length === 0) return;
+
+    this.searchTag(this._tagsHistory[0]);
+  }
+
+  searchTag( tag: string): void {
+    if (tag.length === 0) return;
+
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+                    .set('api_key', this.apiKey)
+                    .set('q', tag)
+                    .set('limit', '10')
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, {params})
+      .subscribe( resp => {
+          this.gifList = resp.data;
+      });
+
+
+    // const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=Lagnve7OTCUw2mIspAQcZCff0Hn7KaAL&q=valorant&limit=10')
+    //                     //.then(resp => resp.json()).then(data => console.log(data));
+    // const data = await resp.json();
+    // console.log(data );
+
+  }
+
+
+
+}
